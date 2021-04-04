74a4ba6cf852abdef751e68d4535be8134d33e81
mjque88 <81833509+mjque88@users.noreply.github.com>
2021-04-04 16:20:33 +1000

    Added working code to Import CSV button. Next to check and amend are the following:  - Data type should be integer for current count column.  - Integer will sort properly rather than string.  - Columns will auto resize.  - Item Code, Item Description and On Order columns to be read only.

--- Wood Stocks App/Form1.cs
+++ Wood Stocks App/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Wood_Stocks_App
@@ -20,10 +15,52 @@ namespace Wood_Stocks_App
 
         private void btnImportCSV_Click(object sender, EventArgs e)
         {
-            openFileDialog1.ShowDialog();
-            openFileDialog1.InitialDirectory = "C:\\StockFile";
-            openFileDialog1.RestoreDirectory = true;
-            File.ReadAllLines(openFileDialog1.FileName);
+            txtFilename.Text = openFileDialog1.FileName;
+           
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                openFileDialog1.InitialDirectory = "C:\\StockFile";
+                openFileDialog1.RestoreDirectory = true;
+                string strFileName = openFileDialog1.FileName;
+                LoadData(strFileName);
+                MessageBox.Show("Import Successful");
+            }
+            else
+            {
+                MessageBox.Show("Import Cancelled");
+            }
+        }
+
+        private void LoadData(string strFilePath)
+        {
+            DataTable dt = new DataTable();
+            string[] lines = System.IO.File.ReadAllLines(strFilePath);
+            if (lines.Length > 0)
+            {
+                //Header
+                string strfirstLine = lines[0];
+                string[] headerLabels = strfirstLine.Split(',');
+                foreach (string strheader in headerLabels)
+                {
+                    dt.Columns.Add(new DataColumn(strheader));
+                }
+                //Data
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] strData = lines[i].Split(',');
+                    DataRow dr = dt.NewRow();
+                    int columnIndex = 0;
+                    foreach (string strheader in headerLabels)
+                    {
+                        dr[strheader] = strData[columnIndex++];
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            if (dt.Rows.Count > 0)
+            {
+                dgvStocklist.DataSource = dt;
+            }
         }
 
         private void btnSaveCSV_Click(object sender, EventArgs e)
@@ -45,5 +82,22 @@ namespace Wood_Stocks_App
         {
 
         }
+
+        private void dgvStocklist_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void txtFilename_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblFilename_Click(object sender, EventArgs e)
+        {
+
+        }
     }
+
+
 }
